099e9b669b975b590099f8d522a7767a3b8940cb
Cordrac <mastroeni06@gmail.com>
2025-12-07 11:56:00 +0000

    fix: correct base path case

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/codrac.github.io/', 
+  base: '/Codrac.github.io/', 
   server: {
     port: 3000,
     host: '0.0.0.0',
